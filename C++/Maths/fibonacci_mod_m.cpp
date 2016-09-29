9f7ff2713a7448ca5e78bad4fd10a371e01a05bd
likecs <f2014028@pilani.bits-pilani.ac.in>
2016-09-30 01:03:17 +0530

    Updated faster method

--- C++/Maths/fibonacci_mod_m.cpp
+++ C++/Maths/fibonacci_mod_m.cpp
@@ -5,21 +5,29 @@ f[n] = f[n-1] + f[n-2]
 */
 
 typedef long long LL;
+const int MAX = 1e5 + 5;
 
-LL n, m, MOD;
- 
-LL mulmod(LL a,LL b, LL m){LL q=(LL)(((long double)a*(long double)b)/(long double)m);LL r=a*b-q*m;if(r>m)r%=m;if(r<0)r+=m;return r;}
- 
-inline LL fib(LL& x, LL& y, LL n) {
-    if (!n) x = 0, y = 1;
+template<typename T> T mod(T a, T b) {return (a<b ? a : a%b);}
+
+int fibo[MAX];
+void pre_compute() {
+    fibo[0] = 0, fibo[1] = 1;
+    for(int i=2; i<MAX; ++i) {
+        fibo[i] = add(fibo[i-1], fibo[i-2], MOD);
+    }
+}
+
+inline int fib(int& x, int& y, LL n) {
+    if (n<1e5) {
+        x = fibo[n], y = fibo[n+1];
+    }
     else {
-        LL a, b;
+        int a, b;
         fib(a, b, n >> 1);
-        LL z = (b << 1) - a;
+        int z = (b << 1) - a;
         if (z < 0) z += MOD;
-        x = mulmod(a, z, MOD);
-        y = mulmod(a, a, MOD) + mulmod(b, b, MOD);
-        if (y >= MOD) y -= MOD;
+        x = mod((LL)a * z, (LL)MOD);
+        y = mod((LL)a * a + (LL)b * b, (LL)MOD);
         if (n & 1) {
             x += y;
             if (x >= MOD) x -= MOD;
@@ -28,8 +36,8 @@ inline LL fib(LL& x, LL& y, LL n) {
     }
     return x;
 }
- 
-inline LL fib(LL n) {
-    LL x = 0, y = 1;
+
+inline int fib(LL n) {
+    int x = 0, y = 1;
     return fib(x, y, n);
 }
