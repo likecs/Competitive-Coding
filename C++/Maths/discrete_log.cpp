1aae82cdfa30476b022d4afa36cca6259d390b38
likecs <f2014028@pilani.bits-pilani.ac.in>
2016-12-01 03:06:39 +0530

    Added new mod algos

---
+++ C++/Maths/discrete_log.cpp
@@ -0,0 +1,62 @@
+//Discrete log implementations
+
+int mod_neg(int a, int b, int c) {
+	LL res = (LL)a * b;
+	if (res < 0) res = (res % c + c);
+	else if (res >= c) res %= c;
+	return res;
+}
+
+int mul(int a, int b, int c) {
+	LL res = (LL)a * b;
+	return (res >= c ? res % c : res);
+}
+
+template<typename T> T extended_euclid(T a, T b, T &x, T &y) {
+	T xx = 0, yy = 1; y = 0; x = 1;
+	while(b) {
+		T q = a / b, t = b;
+		b = a % b; a = t;
+		t = xx; xx = x - q * xx;
+		x = t; t = yy;
+		yy = y - q * yy; y = t;
+	}
+	return a;
+}
+
+//find smallest x such that a^x = b (mod c)
+//returns -1 on failure
+int baby_step_giant_leap(int a, int b, int c) {
+	int temp = 1, cnt = 0, d = 1, x = a, z = b, y;
+	for(int i = 0; i < 32; ++i) {
+		if (temp == b) return i;
+		temp = mul(temp, a, c);
+	}
+	int res = __gcd(a, c);
+	while(res > 1) {
+		if (b % res) return -1;
+		b /= res;
+		c /= res;
+		d = mul(d, a/res, c);
+		res = __gcd(a, c);
+		cnt += 1;
+	}
+	int n = ceil(sqrt(c));
+	int base = 1, v;
+	unordered_map<int,int> vals;
+	for(int i = 0; i < n; ++i) {
+		if (!vals.count(base))
+			vals[base] = i;
+		base = mul(base, a, c);
+	}
+	for (int i = 0; i < n; ++i) {
+		z = extended_euclid(d, c, x, y);
+		v = c / z;
+		x = mod_neg(b, x / z, v);
+		if (vals.count(x)) {
+			return i * n + cnt + vals[x];
+		}
+		d = mul(d, base, c);
+	}
+	return -1;
+}
---
+++ C++/Maths/primitive_root.cpp
@@ -0,0 +1,58 @@
+//Primitive root implementations for general n
+
+typedef long long LL; 
+
+int mul(int a, int b, int c) {
+	LL res = (LL)a * b;
+	return (res >= c ? res % c : res);
+}
+
+int power(int a, int b, int c) {
+	int x = 1, y = a;
+	while(b) {
+		if (b & 1) x = mul(x, y, c);
+		y = mul(y, y, c);
+		b >>= 1;
+	}
+	return x;
+}
+
+int phi(int n) {
+	int up = sqrt(n), ph = n;
+	for(int i = 2; i <= up; ++i) {
+		if (n % i == 0) {
+			ph /= i;
+			ph *= (i - 1);
+			while(n % i == 0) n /= i;
+		}
+		if (n == 1) break;
+	}
+	if (n > 1) {
+		ph /= n;
+		ph *= (n-1);
+	}
+	return ph;
+}
+
+int primitive_root(int p) {
+	vector<int> factors;
+	int n = phi(p), up = sqrt(n);
+	for(int i = 2; i <= up; ++i) {
+		if (n % i == 0) {
+			factors.push_back(i);
+			if (n != i*i) factors.push_back(n/i);
+		}
+	}
+	for(int i = 2; i < p; ++i) {
+		if (__gcd(i, p) != 1) continue;
+		bool done = true;
+		for(auto it : factors) {
+			if (power(i, n / it, p) == 1) {
+				done = false;
+				break;
+			}
+		}
+		if (done) return i;
+	}
+	return -1;
+}
