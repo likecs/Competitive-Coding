fcb0b98eab02b4f7df25c35c8a158577ed18bcd0
likecs <bhuvnesh_jain@ymail.com>
2017-10-16 11:57:26 +0530

    Added map

---
+++ C++14/Data Structs/Map/map.cpp
@@ -0,0 +1,36 @@
+//Implementation of Map to get O(1) access for small values
+//and O(logn) for large values.
+//But it uses larger memory as many small places are uninitialised.
+
+struct MAP {
+	#define LIM 128
+	#define NIL -1
+	int small[LIM];
+	map<int, int> mp;
+	MAP() {
+		mp.clear();
+		for(int i = 0; i < LIM; ++i) {
+			small[i] = NIL;
+		}
+	}
+	bool has(int a) {
+		if (a < LIM) {
+			return (small[a] != NIL);
+		}
+		return mp.find(a) != mp.end();
+	}
+	int value(int a) {
+		if (a < LIM) {
+			return small[a];
+		}
+		return mp[a];
+	}
+	int assign(int a, int b) {
+		if (a < LIM) {
+			small[a] = b;
+		}
+		else {
+			mp[a] = b;
+		}
+	}
+};
