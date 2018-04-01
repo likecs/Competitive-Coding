68b42b413980cdf0079224c712ce9bc38f02b1fd
Aryan Choudhary <32961797+aryanc403@users.noreply.github.com>
2018-04-01 06:07:33 +0530

    mergeSort Added Usage
    mergeSort Algo Added with usage details.

--- C++14/General/Sorting Algorithm/mergeSort.cpp
+++ C++14/General/Sorting Algorithm/mergeSort.cpp
@@ -1,24 +1,26 @@
-//Testing done on https://www.codechef.com/problems/TSORT .
-//https://www.codechef.com/viewsolution/18002412 . Passed.
-#include<vector>
-#include<bits/stdc++.h>
-using namespace std;
-
-//Time Complexicity - O(n logn).
-//Space Complexicity - O(n).
-
-typedef long long int lli;
-void merge(vector <lli> &v,lli startIndex,lli mid,lli endIndex)
+/***
+ *  Merge Sort Algorithm for vector<long long int>.
+ *
+ *  Usage -
+ *  For call use function call-
+ *    sort(<name of vector>,0,<size of vector>);
+ *
+ *  Testing done on https://www.codechef.com/problems/TSORT .
+ *  https://www.codechef.com/viewsolution/18002414 . Passed.
+ *  Time Complexicity - O(n logn).
+ *  Space Complexicity - O(n).
+ ***/
+void merge(vector <long long int> &v, long long int startIndex,long long int mid,long long int endIndex)
 {
-    lli k=startIndex;
-    vector <lli> a;
-    vector <lli> b;
+    long long int k=startIndex;
+    vector <long long int> a;
+    vector <long long int> b;
     a.clear();
     b.clear();
     a.assign(v.begin()+startIndex, v.begin()+mid);
     b.assign(v.begin()+mid, v.begin()+endIndex);
-    lli i=0,l1=mid-startIndex;
-    lli j=0,l2=endIndex-mid;
+    long long int i=0,l1=mid-startIndex;
+    long long int j=0,l2=endIndex-mid;
 
     while(i<l1&&j<l2)
     {
@@ -43,12 +45,13 @@ void merge(vector <lli> &v,lli startIndex,lli mid,lli endIndex)
     }
 }
 
-void sort(vector <lli> &v,lli startIndex,lli endIndex)//[l,r);
+void sort(vector <long long int> &v,long long int startIndex,long long int endIndex)
+//sort v with index in range [l,r).
 {
     if(startIndex>=endIndex-1)
         return;
 
-    lli mid=startIndex+(endIndex-startIndex)/2;
+    long long int mid=startIndex+(endIndex-startIndex)/2;
     sort(v,startIndex,mid);
     sort(v,mid,endIndex);
     merge(v,startIndex,mid,endIndex);
